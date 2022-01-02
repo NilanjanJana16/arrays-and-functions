94d6ef55f8e4864345a68c7feac89247819ad6a9
NilanjanJana16 <nildbs16@gmail.com>
2022-01-02 20:02:51 +0530

    1st commit

---
+++ binarysearch.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class binarysearch {
+
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+        int data = 70;
+
+        int low= 0;
+        int high= arr.length-1;
+        while(low<=high){
+
+            int mid = (low + high)/2;
+            if(data>arr[mid]){
+                low = mid +1;
+
+            }else if(data < arr[mid]){
+
+                high = mid -1;
+
+            }else{
+                System.out.println(mid);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+    
+}
